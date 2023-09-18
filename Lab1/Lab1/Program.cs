c11d944ee9cc8b64a108d1155e08cde5f66832ae
sergiy.kolosov <sergiy.kolosov@DESKTOP-3HPSJDC>
2023-09-18 18:53:39 +0300

    Add endpoint /healthchek,that returns application status and real date

--- Lab1/Lab1/Program.cs
+++ Lab1/Lab1/Program.cs
@@ -1,6 +1,34 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Newtonsoft.Json;
+using System.Net.Mime;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddHealthChecks();
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+app.MapHealthChecks("/healthcheck", new HealthCheckOptions
+{
+    // Customize the response details
+    ResponseWriter = async (context, report) =>
+    {
+        var result = JsonConvert.SerializeObject(
+            new
+            {
+                status = report.Status.ToString(),
+                date = DateTime.UtcNow
+            });
+        context.Response.ContentType = MediaTypeNames.Application.Json;
+        await context.Response.WriteAsync(result);
+    }
+});
+
+app.MapGet("/", () => "Hello user!");
+
+
 
 app.Run();
+
