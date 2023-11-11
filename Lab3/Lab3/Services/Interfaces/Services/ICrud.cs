0d822c0994b2aadaf139140148102305c9b74588
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:18:59 +0200

    Add ICrud interface

---
+++ Lab3/Lab3/Services/Interfaces/Services/ICrud.cs
@@ -0,0 +1,15 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Services.Interfaces.Services
+{
+    public interface ICrud<TModel> where TModel : class
+    {
+        Task<IEnumerable<TModel>> GetAllAsync();
+
+        Task<TModel> GetByIdAsync(int id);
+
+        Task AddAsync(TModel model);
+
+        Task<TModel> DeleteAsync(int modelId);
+    }
+}
