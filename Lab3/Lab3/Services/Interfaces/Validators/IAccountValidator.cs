0fb39ac55631b330aa9b0e0c478ef2caf2ee688a
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:19:57 +0200

    Add IAccountValidator interface

---
+++ Lab3/Lab3/Services/Interfaces/Validators/IAccountValidator.cs
@@ -0,0 +1,9 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Services.Interfaces.Validators
+{
+    public interface IAccountValidator: IValidator<Account>
+    {
+        public Task<ValidationResult> ValidateForTopUpBalance(int account_id, decimal amount);
+    }
+}
