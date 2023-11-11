0fb0292713aaa077f86bbdbe1588d8370fa8db4b
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:16:29 +0200

    Add ValidationResult model

---
+++ Lab3/Lab3/Models/ValidationResult.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Lab3.Models
+{
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public List<string> Messages { get; set; }
+
+        public override string ToString()
+        {
+            return IsValid ? "Validation succeed" : "Validation failed: " + string.Join(", ", Messages);
+        }
+    }
+}
