05dd83433e49520e15a6a428ac39142d9be04fce
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:09:52 +0200

    Add DB context

---
+++ Lab3/Lab3/Database/Lab3DBContext.cs
@@ -0,0 +1,49 @@
+﻿using Lab3.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Lab3.Database
+{
+    public class Lab3DBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Record> Records { get; set; }
+        public DbSet<Account> Accounts { get; set; }
+
+        public Lab3DBContext(DbContextOptions<Lab3DBContext> options) : base(options) { }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Account>()
+                .Property(a => a.Balance)
+                .HasColumnType("decimal(18, 2)");
+
+            modelBuilder.Entity<Record>()
+                .Property(r => r.Amount)
+                .HasColumnType("decimal(18, 2)");
+            modelBuilder.Entity<Account>().HasKey(a => a.Id);
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.Records)
+                .WithOne(r => r.User)
+                .HasForeignKey(r => r.UserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Record>()
+                .HasOne(r => r.Category)
+                .WithMany(c => c.Records)
+                .HasForeignKey(r => r.CategoryId)
+                .OnDelete(DeleteBehavior.SetNull);
+
+            modelBuilder.Entity<Category>()
+                .HasMany(c => c.Records)
+                .WithOne(r => r.Category)
+                .HasForeignKey(r => r.CategoryId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            modelBuilder.Entity<User>()
+                .HasOne(u => u.Account)
+                .WithOne(a => a.User)
+                .HasForeignKey<Account>(a => a.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
