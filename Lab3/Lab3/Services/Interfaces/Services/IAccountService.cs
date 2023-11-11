fd46225cfb42ef0876359a5505edb161d975eacb
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:19:24 +0200

    Add IAccountService interface

---
+++ Lab3/Lab3/Services/Interfaces/Services/IAccountService.cs
@@ -0,0 +1,9 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Services.Interfaces.Services
+{
+    public interface IAccountService: ICrud<Account>
+    {
+        public Task TopUpBalance(int account_id, decimal amount);
+    }
+}
