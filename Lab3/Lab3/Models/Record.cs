2e170eff50510b094f871149095790804eacebeb
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:18:09 +0200

    Add Data Annotations and JSON Ignore Attribute to Record Model

--- Lab3/Lab3/Models/Record.cs
+++ Lab3/Lab3/Models/Record.cs
@@ -1,10 +1,24 @@
-﻿namespace Lab3.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace Lab3.Models
 {
     public class Record: BaseModel
     {
+        [Required]
         public int CategoryId { get; set; }
+        [Required]
         public int UserId { get; set; }
+
+        [Required]
         public DateTime Date { get; set; }
+
+        [Required]
         public decimal Amount { get; set; }
+
+        [JsonIgnore]
+        public virtual Category? Category { get; set; }
+        [JsonIgnore]
+        public virtual User? User { get; set; }
     }
 }
