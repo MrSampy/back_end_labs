f066ab18f34d6574c671f7b244977cfa481b86fc
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:19:46 +0200

    Add IValidator interface

---
+++ Lab3/Lab3/Services/Interfaces/Validators/IValidator.cs
@@ -0,0 +1,10 @@
+﻿using Lab3.Models;
+namespace Lab3.Services.Interfaces.Validators
+{
+    public interface IValidator<TModel> where TModel : BaseModel
+    {
+        public Task<ValidationResult> ValidateForAdd(TModel model);
+        public Task<ValidationResult> ValidateIdAsync(int id);
+
+    }
+}
