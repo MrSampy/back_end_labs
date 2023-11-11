eb71da76a509d9e669cb99b1f1ce6738ef775f69
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:11:11 +0200

    Add UnitOfWork

---
+++ Lab3/Lab3/Database/UnitOfWork/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Database.UnitOfWork
+{
+    public class UnitOfWork: IUnitOfWork
+    {
+        private readonly Lab3DBContext Context;
+
+        private Repository<User> _userRepository;
+        private Repository<Account> _accountRepository;
+        private Repository<Record> _recordRepository;
+        private Repository<Category> _categoryRepository;
+
+        public Repository<User> UserRepository => _userRepository ??= new Repository<User>(Context);
+        public Repository<Account> AccountRepository => _accountRepository ??= new Repository<Account>(Context);
+        public Repository<Record> RecordRepository => _recordRepository ??= new Repository<Record>(Context);
+        public Repository<Category> CategoryRepository => _categoryRepository ??= new Repository<Category>(Context);
+
+        public async Task SaveAsync()
+        {
+            await Context.SaveChangesAsync();
+        }
+
+        public UnitOfWork(Lab3DBContext context)
+        {
+            Context = context;
+        }
+
+    }
+}
