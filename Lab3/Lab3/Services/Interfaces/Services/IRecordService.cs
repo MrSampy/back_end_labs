05c2fd39f4e7c6ae04c0d28a1e00364709eb0b45
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:19:12 +0200

    Add IRecordService interface

---
+++ Lab3/Lab3/Services/Interfaces/Services/IRecordService.cs
@@ -0,0 +1,9 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Services.Interfaces.Services
+{
+    public interface IRecordService: ICrud<Record>
+    {
+        public Task<IEnumerable<Record>> GetFilteredAsync(int? user_id, int? category_id);
+    }
+}
