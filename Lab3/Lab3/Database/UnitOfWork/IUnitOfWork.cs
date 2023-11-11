bcf06213dad44b808c9371afa86e95f76c15a6e9
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:11:04 +0200

    Add IUnitOfWork

---
+++ Lab3/Lab3/Database/UnitOfWork/IUnitOfWork.cs
@@ -0,0 +1,14 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Database.UnitOfWork
+{
+    public interface IUnitOfWork
+    {
+        Repository<User> UserRepository { get; }
+        Repository<Account> AccountRepository { get; }
+        Repository<Record> RecordRepository { get; }
+        Repository<Category> CategoryRepository { get; }
+
+        public Task SaveAsync();
+    }
+}
