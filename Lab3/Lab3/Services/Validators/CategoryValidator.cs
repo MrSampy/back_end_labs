f4c4e40eb0788a187247a35a5ecc5835c3430bec
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:20:52 +0200

    Add CategoryValidator

---
+++ Lab3/Lab3/Services/Validators/CategoryValidator.cs
@@ -0,0 +1,50 @@
+﻿using Lab3.Database.UnitOfWork;
+using Lab3.Models;
+using Lab3.Services.Interfaces.Validators;
+
+namespace Lab3.Services.Validators
+{
+    public class CategoryValidator : IValidator<Category>
+    {
+        protected readonly IUnitOfWork unitOfWork;
+        public CategoryValidator(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public Task<ValidationResult> ValidateForAdd(Category model)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+
+            if(string.IsNullOrEmpty(model.Name))
+            {
+                result.IsValid = false;
+                result.Messages.Add("Name is required");
+            }
+
+            return Task.FromResult(result);
+        }
+
+        public async Task<ValidationResult> ValidateIdAsync(int id)
+        {
+            var result = new ValidationResult()
+            {
+                IsValid = true,
+                Messages = new List<string>()
+            };
+
+            var existingCategory = await unitOfWork.CategoryRepository.GetByIdAsync(id);
+            if (existingCategory == null)
+            {
+                result.IsValid = false;
+                result.Messages.Add("Category not found");
+            }
+
+            return result;
+        }
+    }
+}
