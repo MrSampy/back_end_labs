764dd59b6e5229232a01d5386c0f64560bfd2403
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-11-11 16:20:07 +0200

    Add IRecordValidator interface

---
+++ Lab3/Lab3/Services/Interfaces/Validators/IRecordValidator.cs
@@ -0,0 +1,9 @@
+﻿using Lab3.Models;
+
+namespace Lab3.Services.Interfaces.Validators
+{
+    public interface IRecordValidator : IValidator<Record>
+    {
+        public Task<ValidationResult> ValidateForFiltered(int? user_id, int? category_id);
+    }
+}
