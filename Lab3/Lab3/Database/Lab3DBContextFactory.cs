a2842b2126a27908a5f1d1d1c7c227843bfb3f30
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-12-04 01:18:15 +0200

    Add Lab3DBContextFactory

---
+++ Lab3/Lab3/Database/Lab3DBContextFactory.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Lab3.Database
+{
+    public class Lab3DBContextFactory: IDesignTimeDbContextFactory<Lab3DBContext>
+    {
+        public Lab3DBContext CreateDbContext(string[] args)
+        {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                           .SetBasePath(Directory.GetCurrentDirectory())
+                           .AddJsonFile("appsettings.json")
+                           .Build();
+
+            var builder = new DbContextOptionsBuilder<Lab3DBContext>();
+            var connectionString = configuration.GetConnectionString("DefaultConnection");
+
+            builder.UseSqlServer(connectionString);
+
+            return new Lab3DBContext(builder.Options);
+        }
+    }
+}
