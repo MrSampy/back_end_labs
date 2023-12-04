d33ed6858e19aea485c891de34e7acc7ccbdd5dc
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-12-05 00:24:39 +0200

    Add authorization in user controllera

--- Lab4/Lab4/Controllers/UserController.cs
+++ Lab4/Lab4/Controllers/UserController.cs
@@ -1,7 +1,12 @@
 ﻿using Lab4.Models;
 using Lab4.Services;
 using Lab4.Services.Interfaces.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 using System.Xml.Linq;
 
 namespace Lab4.Controllers
@@ -9,14 +14,55 @@ namespace Lab4.Controllers
     [ApiController]
     public class UserController
     {
-        private readonly ICrud<User> userService;
+        private readonly IUserService userService;
+        private readonly IConfiguration _configuration;
 
-        public UserController(ICrud<User> userService) 
+        public UserController(IUserService userService, IConfiguration configuration) 
         {            
             this.userService = userService;
+            _configuration = configuration;
+        }
+        private string GenerateJwtToken()
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(_configuration["Jwt:key"]);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(),
+                Expires = DateTime.UtcNow.AddHours(1),
+                Issuer = _configuration["Jwt:Issuer"],
+                Audience = _configuration["Jwt:Audience"],
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
+        }
+
+        // POST: api/login
+        [AllowAnonymous]
+        [Route("login")]
+        [HttpPost]
+        public async Task<ActionResult> LogIn([FromBody] User user)
+        {
+            bool result;
+            try
+            {
+                result = await userService.LogIn(user);
+            }
+            catch (Exception ex)
+            {
+                return new BadRequestObjectResult(ex.Message);
+            }
+            if (result)
+            {
+                var tokenString = GenerateJwtToken();
+                return new OkObjectResult(new { token = tokenString });
+            }
+            return new UnauthorizedObjectResult("Wrong password");
         }
 
         //GET: /users
+        [Authorize]
         [Route("users")]
         [HttpGet]
         public async Task<ActionResult<IEnumerable<User>>> GetUsers()
@@ -25,6 +71,7 @@ namespace Lab4.Controllers
         }
 
         //GET: /user/id
+        [Authorize]
         [Route("user/{user_id:int}")]
         [HttpGet]
         public async Task<ActionResult<User>> GetUserById(int user_id)
@@ -42,6 +89,7 @@ namespace Lab4.Controllers
             return new ObjectResult(result);
         }
         //DELETE: /user/user_id
+        [Authorize]
         [Route("user/{user_id:int}")]
         [HttpDelete]
         public async Task<ActionResult<User>> DeleteUserById(int user_id)
@@ -59,6 +107,7 @@ namespace Lab4.Controllers
             return new ObjectResult(result);
         }
         // POST: /user
+        [AllowAnonymous]
         [Route("user")]
         [HttpPost]
         public async Task<ActionResult<User>> AddUser([FromBody] User user)
