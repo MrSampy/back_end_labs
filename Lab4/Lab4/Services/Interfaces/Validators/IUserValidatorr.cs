591f6c1cbd89c77d712bfe7f5ec88cebc67d93a4
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-12-05 00:25:13 +0200

    Add IUserValidatorr

---
+++ Lab4/Lab4/Services/Interfaces/Validators/IUserValidatorr.cs
@@ -0,0 +1,9 @@
+﻿using Lab4.Models;
+
+namespace Lab4.Services.Interfaces.Validators
+{
+    public interface IUserValidatorr: IValidator<User>
+    {
+        public Task<ValidationResult> ValidateForLogIn(User user);
+    }
+}
