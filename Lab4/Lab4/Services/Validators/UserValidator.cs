635e2592b7da4c30496834cd9dad04d8c87b8d23
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-12-05 00:26:07 +0200

    Add ValidateForLogIn in UserValidator

--- Lab4/Lab4/Services/Validators/UserValidator.cs
+++ Lab4/Lab4/Services/Validators/UserValidator.cs
@@ -4,14 +4,15 @@ using Lab4.Services.Interfaces.Validators;
 
 namespace Lab4.Services.Validators
 {
-    public class UserValidator : IValidator<User>
+    public class UserValidator : IUserValidatorr
     {
         protected readonly IUnitOfWork unitOfWork;
         public UserValidator(IUnitOfWork unitOfWork)
         {
             this.unitOfWork = unitOfWork;
         }
-        public Task<ValidationResult> ValidateForAdd(User model)
+
+        private Task<ValidationResult> ValidateEntity(User model) 
         {
             var result = new ValidationResult()
             {
@@ -19,13 +20,47 @@ namespace Lab4.Services.Validators
                 Messages = new List<string>()
             };
 
-            if (string.IsNullOrEmpty(model.Name)) 
+            if (string.IsNullOrEmpty(model.Name))
             {
                 result.IsValid = false;
                 result.Messages.Add("Name is required");
             }
 
+            if (string.IsNullOrEmpty(model.Password))
+            {
+                result.IsValid = false;
+                result.Messages.Add("Password is required");
+            }
+
             return Task.FromResult(result);
+        }
+
+        public async Task<ValidationResult> ValidateForAdd(User model)
+        {
+            var result = await ValidateEntity(model);
+
+            var existingUser = (await unitOfWork.UserRepository.GetAllAsync()).FirstOrDefault(u => u.Name == model.Name);
+            if (existingUser != null)
+            {
+                result.IsValid = false;
+                result.Messages.Add("User with this name already exists");
+            }
+
+            return result;
+        }
+
+        public async Task<ValidationResult> ValidateForLogIn(User user)
+        {
+            var result = await ValidateEntity(user);
+
+            var existingUser = (await unitOfWork.UserRepository.GetAllAsync()).FirstOrDefault(u => u.Name == user.Name);
+            if (existingUser == null)
+            {
+                result.IsValid = false;
+                result.Messages.Add("User not found");
+            }
+
+            return result;
         }
 
         public async Task<ValidationResult> ValidateIdAsync(int id)
