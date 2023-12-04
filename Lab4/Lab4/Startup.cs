c9bd462ffa8b237bcd5e9cb892c5f82c017e4ed9
mynameisserzheo@gmail.com <mynameisserzheo@gmail.com>
2023-12-05 00:26:59 +0200

    Update startup file

--- Lab4/Lab4/Startup.cs
+++ Lab4/Lab4/Startup.cs
@@ -5,12 +5,15 @@ using Lab4.Services;
 using Lab4.Services.Interfaces.Services;
 using Lab4.Services.Interfaces.Validators;
 using Lab4.Services.Validators;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.InMemory;
+using Microsoft.IdentityModel.Tokens;
 using Newtonsoft.Json;
 using System.Net.Mime;
+using System.Text;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace Lab4
@@ -30,6 +33,7 @@ namespace Lab4
             var optionsBuilder = new DbContextOptionsBuilder<Lab4DBContext>().UseNpgsql(connection, sqlServerOptions => sqlServerOptions.EnableRetryOnFailure());
 
             var options = optionsBuilder.Options;
+
             using (var context = new Lab4DBContext(options))
             {
                 var canConnect = false;
@@ -54,13 +58,13 @@ namespace Lab4
                     SeedData(context);
                 }
             }
-
+            
             var unitOfWork = new UnitOfWork(new Lab4DBContext(options));
 
             services.AddDbContext<Lab4DBContext>(options =>
             {
                 options.UseNpgsql(connection, sqlServerOptions => sqlServerOptions.EnableRetryOnFailure());
-            });            
+            });   
 
             services.AddSingleton<IUnitOfWork>(x => unitOfWork);
 
@@ -70,7 +74,7 @@ namespace Lab4
 
             services.AddTransient<IRecordValidator, RecordValidator>();
 
-            services.AddTransient<IValidator<User>, UserValidator>();
+            services.AddTransient<IUserValidatorr, UserValidator>();
 
             services.AddTransient<IValidator<Category>, CategoryValidator>();
 
@@ -84,7 +88,7 @@ namespace Lab4
 
             services.AddTransient<IAccountService, AccountService>();
 
-            services.AddTransient<ICrud<User>, UserService>();
+            services.AddTransient<IUserService, UserService>();
 
             services.AddTransient<ICrud<Category>, CategoryService>();
 
@@ -95,19 +99,50 @@ namespace Lab4
             services.AddHealthChecks();
 
             services.AddEndpointsApiExplorer();
+
+            #region Authentication
+            services.AddAuthorization(options =>
+            {
+                var defaultAuthorizationPolicyBuilder = new AuthorizationPolicyBuilder(
+                    JwtBearerDefaults.AuthenticationScheme);
+
+                defaultAuthorizationPolicyBuilder =
+                    defaultAuthorizationPolicyBuilder.RequireAuthenticatedUser();
+
+                options.DefaultPolicy = defaultAuthorizationPolicyBuilder.Build();
+            });
+            services.AddAuthentication(option =>
+            {
+                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = false,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = Configuration["Jwt:Issuer"],
+                    ValidAudience = Configuration["Jwt:Audience"],
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["Jwt:Key"])) 
+                };
+            });
+            #endregion
         }
 
         protected void SeedData(Lab4DBContext context) 
         {
             context.Users.AddRange(
-                new User { Name = "Користувач 1" },
-                new User { Name = "Користувач 2" },
-                new User { Name = "Користувач 3" },
-                new User { Name = "Користувач 4" },
-                new User { Name = "Користувач 5" },
-                new User { Name = "Користувач 6" },
-                new User { Name = "Користувач 7" },
-                new User { Name = "Користувач 8" }
+                new User { Name = "1", Password = SecurePasswordHasher.Hash("1") }, 
+                new User { Name = "2", Password = SecurePasswordHasher.Hash("123") },
+                new User { Name = "3", Password = SecurePasswordHasher.Hash("qwe") },
+                new User { Name = "4", Password = SecurePasswordHasher.Hash("asdfds") },
+                new User { Name = "5", Password = SecurePasswordHasher.Hash("21") },
+                new User { Name = "6", Password = SecurePasswordHasher.Hash("532") },
+                new User { Name = "7", Password = SecurePasswordHasher.Hash("2346") },
+                new User { Name = "8", Password = SecurePasswordHasher.Hash("75") }
             );
             context.SaveChanges();
             context.Categories.AddRange(
@@ -153,6 +188,8 @@ namespace Lab4
             app.UseHttpsRedirection();            
 
             app.UseRouting();
+
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints => {
                 endpoints.MapControllers();
